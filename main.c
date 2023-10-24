64690f7dc4faa0ecfc0523e6ffce6d3307336e21
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-10-24 14:49:14 -0500

    1

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-#include <stdio.h>>
 #include <stdio.h>
 
 int main() {
@@ -7,37 +6,5 @@ int main() {
     int numCeros = 0;
     int numPositivos = 0;
     int numNegativos = 0;
-
-    printf("Ingrese la cantidad de números (N): ");
-    scanf("%d", &N);
-
-    if (N <= 0) {
-        printf("La cantidad de números debe ser como positivos o negativos \n");
-        return 1;
-    }
-
-    printf("Ingrese los %d números:\n", N);
-
-    for (int i = 0; i < N; i++) {
-        int num;
-        printf("Número %d: ", i + 1);
-        scanf("%d", &num);
-
-        if (num == 0) {
-            numCeros++;
-        } else if (num > 0) {
-            numPositivos++;
-        } else {
-            numNegativos++;
-        }
-
-        totalNumeros++;
-    }
-
-    printf("Porcentaje de números cero: %.2f%%\n", (float)numCeros / totalNumeros * 100);
-    printf("Porcentaje de números positivos: %.2f%%\n", (float)numPositivos / totalNumeros * 100);
-    printf("Porcentaje de números negativos: %.2f%%\n", (float)numNegativos / totalNumeros * 100);
-
-    return 0;
+    
 }
-
