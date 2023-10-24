f7ef2f83cf94b9a6adab02f19f7eb9c7d13ea5d5
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2023-10-24 14:50:24 -0500

    2

--- main.c
+++ main.c
@@ -6,5 +6,14 @@ int main() {
     int numCeros = 0;
     int numPositivos = 0;
     int numNegativos = 0;
+    printf("Ingrese la cantidad de números (N): ");
+    scanf("%d", &N);
+
+    if (N <= 0) {
+        printf("La cantidad de números debe ser un entero positivo.\n");
+        return 1;
+    }
     
+
+    return 0;
 }
